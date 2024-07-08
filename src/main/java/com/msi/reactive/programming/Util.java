74dc5ab819a84616403ac8378581367fba8ee583
YeganehNobakht <m.yeganeh.n1994@gmail.com>
2024-07-08 00:43:00 -0700

    adding util class

---
+++ src/main/java/com/msi/reactive/programming/Util.java
@@ -0,0 +1,39 @@
+package com.msi.reactive.programming;
+
+import com.github.javafaker.Faker;
+
+import java.util.function.Consumer;
+
+public class Util {
+
+    private static final Faker FAKER = Faker.instance();
+
+    public static Consumer<Object> onNext(){
+        return o -> System.out.println("Received : " + o);
+    }
+
+    public static Consumer<Throwable> onError(){
+        return e -> System.out.println("ERROR : " + e.getMessage());
+    }
+
+    public static Runnable onComplete(){
+        return () -> System.out.println("Completed");
+    }
+
+    public static Faker faker(){
+        return FAKER;
+    }
+
+    public static void sleepSeconds(int seconds){
+        sleepMillis(seconds * 1000);
+    }
+
+    public static void sleepMillis(int millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
